907364d31638d27fe2c8428085e029d68718f994
Epsilon10 <benjamin.oommen1@gmail.com>
2018-01-03 06:29:09 +0530

    tic tac toe

--- cogs/TTT.py
+++ cogs/TTT.py
@@ -4,9 +4,9 @@ from discord.ext import commands
 class TTT:
     def __init__(self):
         self.current_board = [
-            ['X',' ',' '],
-            [' ','Z',' '],
-            ['A',' ',' ']
+            [' ',' ',' '],
+            [' ',' ',' '],
+            [' ',' ',' ']
         ]
 
 
@@ -20,5 +20,19 @@ class TTT:
         board += '+---+---+---+'
         print(board)
         return board
+
+    def win_conditions(self):
+        for x in range(3):
+            if self.checkEqual2(self.current_board[x]) and ' ' not in self.current_board[x]:
+                return True
+            elif self.current_board[x][0] == self.current_board[x][1] == self.current_board[x][2] and ' ' != self.current_board[x][0]:
+                return True
+        if ((self.current_board[0][0] == self.current_board[1][1] == self.current_board[2][2]) and self.current_board[0][0] != ' ') or  ((self.current_board[0][2] == self.current_board[1][1] == self.current_board[2][0]) and self.current_board[0][2] != ' '):
+            return True
+        return False
+
+
+    def checkEqual2(iterator):
+        return len(set(iterator)) <= 1
 
         
--- cogs/tictactoe.py
+++ cogs/tictactoe.py
@@ -16,40 +16,60 @@ class TicTacToe:
     @tictactoe.command()
     async def start(self, ctx, p2: discord.Member):
         p1 = ctx.message.author
-        ttt=TTT(ctx.message.author.id,p2.id)
+        ttt=TTT()
         board = await ttt.draw_board_async()
-        prev_board = await ctx.channel.send(f'```{board}```')
-
-        for x in range(3):
-            await ctx.channel.send(f'{p1.mention} it is your turn.', delete_after=5.0)
+        prev_board = await ctx.send(f'```{board}```')
+        chan = ctx.channel
+        for x in range(10):
+            await ctx.channel.send(f'{p1.mention} it is your turn.', delete_after=2.0)
             move = None
-            async def check(m):
+            def check(m):
+                print('Check')
+                loop = asyncio.get_event_loop()
                 rex = re.compile('[0-3][\,][0-3]')
                 if rex.match(m.content):
                     return True
-                await ctx.channel.send('Invalid move or format',delete_after=7.0)
-                await m.delete()
+                if m.content is not None and m.author != self.bot.user and m.channel == chan:
+                    try:
+                        print('bad ' + m.content)
+                        loop.create_task(self.fail(ctx))
+                    except Exception as e:
+                        print('bad: '+m.content)
+                        print(str(e))
                 return False
             try:
                 msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-                move = (msg[1]-1,msg[0]-1)
+
+                msg_stuff = msg.content.split(',')
+                await msg.delete()
+                move = [int(msg_stuff[1])-1,int(msg_stuff[0])-1]
+
+
             except asyncio.TimeoutError:
                 await ctx.channel.send(f'{p2.mention} wins because the opponent forfeited')
                 break
 
+            print('Gone through')
             board = await ttt.draw_board_async(move,'X')
-            prev_board = await prev_board.edit(content=f'```{board}```')
+            await prev_board.edit(content=f'```{board}```')
 
-            await ctx.channel.send(f'{p2.mention} if is your turn.')
+            await ctx.channel.send(f'{p2.mention} if is your turn.', delete_after=1.0)
             try:
                 msg = await self.bot.wait_for('message', timeout=15.0, check=check)
-                move = (msg[1]-1,msg[0]-1)
+                msg_stuff = msg.content.split(',')
+                await msg.delete()
+                move = [int(msg_stuff[1])-1,int(msg_stuff[0])-1]
+
             except asyncio.TimeoutError:
                 await ctx.channel.send(f'{p1.mention} wins because the opponent forfeited')
                 break
 
             board = await ttt.draw_board_async(move,'O')
-            prev_board = await prev_board.edit(content=f'```{board}```')
+            await prev_board.edit(content=f'```{board}```')
+
+    async def fail(self,ctx):
+        await ctx.send('Invalid move please try again', delete_after=3.0)
+
 
 
      
